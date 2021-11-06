c37923962ae5dacdd6ad2669b85a378e9a0c265a
skindervik <38836685+skindervik@users.noreply.github.com>
2021-11-06 18:11:23 +0200

    Firebase Registration added

--- app/src/main/java/com/example/dragonsden/RegisterActivity.java
+++ app/src/main/java/com/example/dragonsden/RegisterActivity.java
@@ -1,32 +1,67 @@
 package com.example.dragonsden;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        auth = FirebaseAuth.getInstance();
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.btn_register:
-                Toast.makeText(this, "TEST", Toast.LENGTH_SHORT).show();
+            {
+                EditText email = findViewById(R.id.register_email);
+                EditText password = findViewById(R.id.register_password);
+                EditText password_confirm = findViewById(R.id.register_password_confirm);
+
+                String text_email = email.getText().toString();
+                String text_password = password.getText().toString();
+                String text_password_confirm = password.getText().toString();
+
+                if (!text_password.equals(text_password_confirm)){
+                    password.setError("Password's did not match");
+                }
+                else{
+                    auth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                            .addOnCompleteListener(RegisterActivity.this, task -> {
+                                if (task.isSuccessful()){
+                                    Toast.makeText(RegisterActivity.this, "Registration was successful!", Toast.LENGTH_SHORT).show();
+                                }
+                                else{
+                                    Toast.makeText(RegisterActivity.this, "Registration failed - "+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                }
+            }
                 break;
             case R.id.btn_link_login:
-//                Intent i = new Intent(this, MainActivity.class);
-//                startActivity(i);
-                this.finish();
+                finish();
                 break;
             default:
                 break;
         }
+    }
+
+
+    private void registerUser(){
+
     }
 }
