e994230a3f7a22fc40eda868a478203979b1759e
skindervik <38836685+skindervik@users.noreply.github.com>
2021-11-06 15:32:14 +0200

    Login/Register ui

--- app/src/main/java/com/example/dragonsden/RegisterActivity.java
+++ app/src/main/java/com/example/dragonsden/RegisterActivity.java
@@ -2,28 +2,28 @@ package com.example.dragonsden;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
-            case R.id.btn_login:
+            case R.id.btn_register:
                 Toast.makeText(this, "TEST", Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.btn_link_register:
-                Intent i = new Intent(this, RegisterActivity.class);
-                startActivity(i);
+            case R.id.btn_link_login:
+//                Intent i = new Intent(this, MainActivity.class);
+//                startActivity(i);
+                this.finish();
                 break;
             default:
                 break;
